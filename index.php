a34252ad005662b1ffefbd1ab9092adabc3b6e18
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2023-06-20 18:46:08 +0200

    delete home.php plus reedit index.php

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
-<?php require_once 'templates/header.php'; ?>
+<?php
+require_once('templates/header.php');
+require_once('lib/recipe.php');
 
-<?php require_once 'templates/home.php'; ?>
+// $recipes = getRecipes($pdo, _HOME_RECIPES_LIMIT_);
 
-<?php require_once 'templates/recipe_partial.php'; ?>
+?>
+<main>
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="assets/images/at-work.jpg" class="d-block mx-lg-auto img-fluid" alt="Logo du site" width="400" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Cuisinez comme vous êtes !</h1>
+                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <a href="recettes.php" class="btn btn-primary btn-lg px-4 me-md-2">Voir nos recettes</a>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row text-center">
 
-<?php require_once 'templates/footer.php'; ?>
+        <?php foreach ($recipes as $key => $recipe) {
+            include('templates/recipe_partial.php');
+        } ?>
 
+
+    </div>
+
+    <?php
+    require_once('templates/footer.php');
+    ?>
--- lib/config.php
+++ lib/config.php
@@ -7,3 +7,4 @@ $mainMenu = [
     'recettes.php' => 'Nos recettes',
     
 ];
+
