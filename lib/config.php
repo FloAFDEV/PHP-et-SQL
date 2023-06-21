540ea3c021666961ca5270bcc89e1437889a3bdb
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2023-06-21 22:50:55 +0200

    Add tools file and aggregate php code with php

--- lib/config.php
+++ lib/config.php
@@ -1,10 +1,13 @@
 <?php
 //création d'une constantes pour le chemin des images
 define('_RECIPES_IMG_PATH_', 'uploads/recipes/');
+define('_ASSETS_IMG_PATH_', 'assets/images/');
 
+//création d'une constante pour le nombre d'entités dans le menu affichées sur la page d'accueil
 $mainMenu = [
     'index.php' => 'Accueil',
     'recettes.php' => 'Nos recettes',
-    
+
 ];
+
 
--- recette.php
+++ recette.php
@@ -1,6 +1,7 @@
 <?php
 require_once('templates/header.php');
 require_once('lib/recipe.php');
+require_once('lib/tools.php');
 
 // var_dump($recipes[$id]); //affiche le tableau de la recette selectionnée
 
@@ -8,23 +9,55 @@ $pdo = new PDO('mysql:host=localhost;dbname=Cuisine;charset=utf8', 'root', 'root
 
 $id = $_GET['id'];
 
-
 $query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
 $query->bindParam(':id', $id, PDO::PARAM_INT);
 $query->execute();
 $recipe = $query->fetch();
-
 // var_dump($recipe);
 
+
+// Si l'image est null, on affiche une image par défaut
+if ($recipe['image'] === null) {
+    $imagePath = _ASSETS_IMG_PATH_ . 'recipe_default.jpg';
+} else
+// Sinon on affiche l'image de la recette
+{
+    $imagePath = _RECIPES_IMG_PATH_ . $recipe['image'];
+}
+
+$ingredients = linesToArray($recipe['ingredients']);
+$instructions = linesToArray($recipe['instructions']);
+
+
 ?>
+
+
+
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-        <img src="<?= _RECIPES_IMG_PATH_ . $recipe['image']; ?>" class="d-block mx-lg-auto img-fluid" alt="" width="700" height="500" loading="lazy">
+        <img src="<?= $imagePath; ?>" class="d-block mx-lg-auto img-fluid" alt="<?= $recipe['title'] ?>" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
         <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?= $recipe['title'] ?></h1>
         <p class="lead"><?= $recipe['description'] ?></p>
     </div>
+</div>
+
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <h2>Ingredients</h2>
+    <ul class="list-group">
+        <?php foreach ($ingredients as $key => $ingredient) { ?>
+            <li class="list-group-item"><?= $ingredient ?></li>
+        <?php } ?>
+    </ul>
+</div>
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <h2>Instructions</h2>
+    <ol class="list-group">
+        <?php foreach ($instructions as $key => $instruction) { ?>
+            <li class="list-group-item"><?= $instruction ?></li>
+        <?php } ?>
+    </ol>
 </div>
 
 <?php require_once('templates/footer.php'); ?>
