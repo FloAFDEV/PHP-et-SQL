66b806791a562abde4c2ddd71a0f43ba1a7cff4c
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2023-06-26 13:14:01 +0200

    Add form & function saveRecipe

--- ajout_modification_recette.php
+++ ajout_modification_recette.php
@@ -3,7 +3,48 @@ require_once('templates/header.php');
 require_once('lib/recipe.php');
 require_once('lib/tools.php');
 
+if (isset($_POST['saveRecipe'])) {
+    saveRecipe($pdo, $_POST['category'], $_POST['title'], $_POST['description'], $_POST['ingredients'], $_POST['instructions'], $_FILES['file']);
+}
+
+
+// var_dump($_POST);
+
 // var_dump($recipes[$id]); //affiche le tableau de la recette selectionnée
+?>
 
+<form method="POST" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="title" class="form-label">Titre</label>
+        <input type="text" name="title" id="title" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea name="description" id="description" cols="30" rows="5" class="form-control"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="ingredients" class="form-label">Ingredients</label>
+        <textarea name="ingredients" id="ingredients" cols="30" rows="5" class="form-control"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="instructions" class="form-label">Instructions</label>
+        <textarea name="instructions" id="instructions" cols="30" rows="5" class="form-control"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="category" class="form-label">Catégorie</label>
+        <select name="category" id="category" class="form-control">
+            <option value="1">Entrée</option>
+            <option value="2">Plat</option>
+            <option value="3">Dessert</option>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label for="file" class="form-label">Image</label>
+        <input type="file" name="file" id="file" class="form-control">
+    </div>
+    <input type="submit" value="Ajouter" name="saveRecipe" class="btn btn-primary">
+</form>
 
+<?php
 require_once('templates/footer.php');
+?>
